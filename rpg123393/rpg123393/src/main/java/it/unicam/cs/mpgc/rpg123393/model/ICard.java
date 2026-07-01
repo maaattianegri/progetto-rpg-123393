ca7f87b4742a423a4b4ef72b59aba6dc4dcf25fb
Mattia <mattia.negri@studenti.unicam.it>
2026-07-01 18:08:39 +0200

    cleanup: rimozione cartella annidata

--- rpg123393/rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/ICard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model;
-
-public interface ICard {
-    String getName();
-    int getManaCost();
-
-    void play(GameCharacter user, GameCharacter target);
-    String getImagePath();
-}
