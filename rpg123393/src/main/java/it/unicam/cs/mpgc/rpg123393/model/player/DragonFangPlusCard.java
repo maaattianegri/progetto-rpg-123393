e9cb054939a6f4e98458cbc1e2cb144983c7c6a4
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:35:52 +0200

    refactor: rimuovi DragonFangPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/DragonFangPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new DragonFangCard(true)}.
- */
-@Deprecated
-public class DragonFangPlusCard extends DragonFangCard {
-    public DragonFangPlusCard() { super(true); }
-}
