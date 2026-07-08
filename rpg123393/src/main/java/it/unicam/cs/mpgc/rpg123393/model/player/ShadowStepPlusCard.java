db8fc6631b70333b4f6808c31606301ad5491c1c
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:37:10 +0200

    refactor: rimuovi ShadowStepPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/ShadowStepPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new ShadowStepCard(true)}.
- */
-@Deprecated
-public class ShadowStepPlusCard extends ShadowStepCard {
-    public ShadowStepPlusCard() { super(true); }
-}
