505472a58a2bcc25eeb38bac141472eacf737bf7
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:36:13 +0200

    refactor: rimuovi FrostboltPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/FrostboltPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new FrostboltCard(true)}.
- */
-@Deprecated
-public class FrostboltPlusCard extends FrostboltCard {
-    public FrostboltPlusCard() { super(true); }
-}
