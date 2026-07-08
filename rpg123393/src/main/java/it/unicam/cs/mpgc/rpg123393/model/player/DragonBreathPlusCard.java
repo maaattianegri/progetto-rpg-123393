119a602f5a6319316e0168cc05af8ee478a26bfa
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:34:42 +0200

    refactor: rimuovi DragonBreathPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/DragonBreathPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new DragonBreathCard(true)}.
- */
-@Deprecated
-public class DragonBreathPlusCard extends DragonBreathCard {
-    public DragonBreathPlusCard() { super(true); }
-}
