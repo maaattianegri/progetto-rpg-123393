5aa147686875fa114a3ae3cb02f9cb17fcda042a
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:36:39 +0200

    refactor: rimuovi ManaShieldPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/ManaShieldPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new ManaShieldCard(true)}.
- */
-@Deprecated
-public class ManaShieldPlusCard extends ManaShieldCard {
-    public ManaShieldPlusCard() { super(true); }
-}
