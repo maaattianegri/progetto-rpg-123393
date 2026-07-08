3733799adcc4af2ff7bff2633db1d8397683002e
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:33:31 +0200

    refactor: rimuovi BerserkerRagePlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/BerserkerRagePlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new BerserkerRageCard(true)}.
- */
-@Deprecated
-public class BerserkerRagePlusCard extends BerserkerRageCard {
-    public BerserkerRagePlusCard() { super(true); }
-}
