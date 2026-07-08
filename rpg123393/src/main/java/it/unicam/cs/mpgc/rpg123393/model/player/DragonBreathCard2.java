28db28a647ae09659d84c648a7cc46fe025e69ed
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:34:35 +0200

    refactor: rimuovi DragonBreathCard2 @Deprecated (duplicato di DragonBreathCard)

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/DragonBreathCard2.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Duplicato di {@link DragonBreathCard}. Usa {@code new DragonBreathCard()} al suo posto.
- */
-@Deprecated
-public class DragonBreathCard2 extends DragonBreathCard {
-    public DragonBreathCard2() { super(false); }
-}
