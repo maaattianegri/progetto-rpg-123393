e15f284c4dab1c74af89f29fb05eb65b33ee662b
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:34:26 +0200

    refactor: rimuovi DoubleBladPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/DoubleBladPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new DoubleBladCard(true)}.
- */
-@Deprecated
-public class DoubleBladPlusCard extends DoubleBladCard {
-    public DoubleBladPlusCard() { super(true); }
-}
