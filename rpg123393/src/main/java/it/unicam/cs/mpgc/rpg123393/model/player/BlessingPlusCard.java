dec294a3b2f9e2aebc99fd30107b1085b7d9320b
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:33:37 +0200

    refactor: rimuovi BlessingPlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/BlessingPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new BlessingCard(true)}.
- */
-@Deprecated
-public class BlessingPlusCard extends BlessingCard {
-    public BlessingPlusCard() { super(true); }
-}
