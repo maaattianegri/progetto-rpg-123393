e5279bc2ebe701213bd80c1224d972fc7657691d
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:33:16 +0200

    refactor: rimuovi ArcaneStormPlusCard @Deprecated (usa ArcaneStormCard(true))

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/ArcaneStormPlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new ArcaneStormCard(true)}.
- */
-@Deprecated
-public class ArcaneStormPlusCard extends ArcaneStormCard {
-    public ArcaneStormPlusCard() { super(true); }
-}
