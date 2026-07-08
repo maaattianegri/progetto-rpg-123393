1400b0e7246f314cf66d8ea31f712749a6b5ef6c
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:36:20 +0200

    refactor: rimuovi HammerOfJusticePlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/HammerOfJusticePlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new HammerOfJusticeCard(true)}.
- */
-@Deprecated
-public class HammerOfJusticePlusCard extends HammerOfJusticeCard {
-    public HammerOfJusticePlusCard() { super(true); }
-}
