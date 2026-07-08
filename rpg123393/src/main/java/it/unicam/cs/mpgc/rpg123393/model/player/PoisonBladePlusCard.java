0e07905973d08bee901f03d705a2abd552c0fb08
maaattianegri <mattia.negri@studenti.unicam.it>
2026-07-08 20:36:47 +0200

    refactor: rimuovi PoisonBladePlusCard @Deprecated

--- rpg123393/src/main/java/it/unicam/cs/mpgc/rpg123393/model/player/PoisonBladePlusCard.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg123393.model.player;
-
-/**
- * @deprecated Usa {@code new PoisonBladeCard(true)}.
- */
-@Deprecated
-public class PoisonBladePlusCard extends PoisonBladeCard {
-    public PoisonBladePlusCard() { super(true); }
-}
